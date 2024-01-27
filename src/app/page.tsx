5d2c25ca317d509bd0e465cef120ffb17ed176da
AnshV2 <misteransh@gmail.com>
2024-01-27 14:32:43 -0500

    initial state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,38 @@
+'use client'
+
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
+import {useState, useEffect} from "react"
 
-export default async function Home() {
+const champsDB = ['Bowswer', 'Captain Falcon', 'Diddy Kong', 'Donkey Kong', 'Falco', 'Fox', 'Ganondorf', 'Ice Climbers', 'Ike', 'Jigglypuff', 'King Dedede', 'Kirby', 'Link', 'Lucario',
+'Lucas', 'Luigi', 'Mario', 'Marth', 'Meta Knight', 'Mr. Game & Watch', 'Ness', 'Olimar', 'Peach', 'Pikachu', 'Pit', 'Pokemon Trainer', 'ROB', 'Samus', 'Sheik', 'Snake', 'Sonic',
+ 'Toon Link', 'Wario', 'Wolf', 'Yoshi', 'Zelda']
+const champsImg = ['Bowser', 'Captain', 'Diddy', 'Donkey', 'Falco', 'Fox', 'Ganondorf', 'Ice', 'Ike', 'Jigglypuff', 'King', 'Kirby', 'Link', 'Lucario',
+'Lucas', 'Luigi', 'Mario', 'Marth', 'Meta', 'Game', 'Ness', 'Olimar', 'Peach', 'Pikachu', 'Pit', 'Pokemon', 'ROB', 'Samus', 'Sheik', 'Snake', 'Sonic', 'Toon', 
+'Wario', 'Wolf', 'Yoshi', 'Zelda', 'Bowser']
+const numChamps = 36;
+
+
+
+
+export default function Home() {
   noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const [player, upPlayer] = useState(Math.floor(Math.random() * numChamps))
+  const {data} = api.post.getOpponent.useQuery({text: champsImg[player] ?? ""})
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <div className = "flex h-screen w-screen bg-slate-200 items-center justify-center">
+      {data && <div className = "flex w-full h-2/3 items-center justify-center">
+        <div className = "relative h-1/2 w-1/3 l-1/9 justify-center items-center  ">
+          <img className = "w-full h-full object-contain" src = {"/Brawlers/" + champsImg[player] + ".jpg"}></img>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
+        <div className = "h-1/2 w-1/3 r-1/9 justify-center items-center   ">
+          <img className = "w-full h-full object-contain" src = {"/Brawlers/" + champsImg[data] + ".jpg"}></img>
         </div>
-
-        <CrudShowcase />
-      </div>
-    </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
+      </div>}
     </div>
   );
 }
+
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,26 +2,28 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const champsDB = ['Bowswer', 'Captain Falcon', 'Diddy Kong', 'Donkey Kong', 'Falco', 'Fox', 'Ganondorf', 'Ice Climbers', 'Ike', 'Jigglypuff', 'King Dedede', 'Kirby', 'Link', 'Lucario',
+'Lucas', 'Luigi', 'Mario', 'Marth', 'Meta Knight', 'Mr. Game & Watch', 'Ness', 'Olimar', 'Peach', 'Pikachu', 'Pit', 'Pokemon Trainer', 'ROB', 'Samus', 'Sheik', 'Snake', 'Sonic',
+ 'Toon Link', 'Wario', 'Wolf', 'Yoshi', 'Zelda']
+
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
+  getOpponent: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
+    .query(async ({ ctx, input }) => {
+      var list = await ctx.db.champs.findMany()
+      var temp = 0;
+      for (let i = 0; i < list.length; i++) {
+        if (input.text === list[i]?.name) {
+          temp = list[i]?.elo ?? 0;
+        }
+      }
+      list = list.map((e) => {return ({name:e.name, elo: e.elo - temp})})
+      list.sort();
+      const rand = Math.floor(Math.random() * 5) + 1
+      const answer = list[rand]?.name ?? ""
+      for (var i = 0; i < champsDB.length; i++) {
+        if (champsDB[i] === answer) {return i;}
+      }
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
